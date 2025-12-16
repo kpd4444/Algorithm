382544900e3dfa9b904dae373ed0164d774387a2
kpd4444 <kpd4444@naver.com>
2025-12-16 17:19:40 +0900

    [Silver III] Title: 두 수의 합, Time: 772 ms, Memory: 109848 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt(); //재료의 갯수
+         //갑옷을 만드는데 필요한 수
+
+        int []s = new int[n];
+
+        //일단 2개를 합쳐서 m이 되는게 몇개인지 판단해야함
+        for (int i = 0; i < n; i++) {
+            s[i] = input.nextInt();
+        }
+        Arrays.sort(s);
+        int m = input.nextInt();
+        int start = 0;
+        int end = n-1;
+
+        int count = 0;
+        while(start < end){
+            int current = s[start] + s[end];
+
+            if(current < m){
+                start++;
+            }
+            else if (current > m){
+                end--;
+            }
+            else{
+                count++;
+                start++;
+            }
+        }
+
+        System.out.println(count);
+
+        input.close();
+    }
+}

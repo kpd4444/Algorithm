eb2a35891bc914c6e1f3de6638ac1e42902dcd66
kpd4444 <kpd4444@naver.com>
2026-04-29 10:44:07 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        String s = String.valueOf(n);
+        for(int i = 0; i < s.length(); i++){
+            sum += (s.charAt(i) - '0');
+        }
+        
+        return sum;
+    }
+}

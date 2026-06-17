b45023848ff1b2d86424b2056134d5a44e1b5f51
kpd4444 <kpd4444@naver.com>
2026-06-17 15:51:27 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ Java/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        if(my_string.startsWith(is_prefix)){
-            return 1;
-        }
-    
-        return 0;
+        int answer = 0;
+        return answer;
     }
 }

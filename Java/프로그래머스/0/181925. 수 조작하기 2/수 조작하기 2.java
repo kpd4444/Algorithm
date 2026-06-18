1fb894e989a3f6e455de881cadf53a50cffd520f
kpd4444 <kpd4444@naver.com>
2026-06-18 17:46:08 +0900

    [level 0] Title: 수 조작하기 2, Time: 4.13 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < numLog.length - 1; i++){
+            int num = numLog[i + 1] - numLog[i];
+            switch (num){
+                case 1:
+                    sb.append("w");
+                    break;
+                case -1:
+                    sb.append("s");
+                    break;
+                case 10:
+                    sb.append("d");
+                    break;
+                case -10:
+                    sb.append("a");
+                    break;
+            }
+            
+        }
+        return sb.toString();
+    }
+}

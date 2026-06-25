430daba8c5d4beabae305d68a76030436aa81f1a
kpd4444 <kpd4444@naver.com>
2026-06-25 16:11:52 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int count = 0;
+        for(int i = 4; i <= n ; i++){
+            for(int j = 2; j < i; j++){
+                if(i % j ==0){
+                    count++;
+                    break;
+                }
+            }
+            
+        }
+        return count;
+    }
+}

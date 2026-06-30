ea3589142b11940a5dd31589e9893f6e15da77aa
kpd4444 <kpd4444@naver.com>
2026-06-30 22:05:43 +0900

    [level 0] Title: 글자 지우기, Time: 0.17 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        for(int i : indices){
+            sb.setCharAt(i, 'A');
+        }
+        String result = sb.toString();
+        
+        
+        return result.replaceAll("A","");
+    }
+}

2b91f30b2131ffd458001ef8f1e8dc04ec3d8179
kpd4444 <kpd4444@naver.com>
2026-07-21 16:12:41 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 111.37 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+        int n = input.nextInt();
+        for(int i = 0; i < n ; i++){
+            System.out.print(s);
+        }
+    }
+}

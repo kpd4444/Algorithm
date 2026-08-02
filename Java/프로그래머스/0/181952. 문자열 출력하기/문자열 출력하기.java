e643a92e6dfff8fe68ed380c1ca4e326acbbd986
kpd4444 <kpd4444@naver.com>
2026-08-02 15:36:22 +0900

    [level 0] Title: 문자열 출력하기, Time: 86.23 ms, Memory: 73 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String a = input.next();
+        
+        System.out.print(a);
+    }
+}

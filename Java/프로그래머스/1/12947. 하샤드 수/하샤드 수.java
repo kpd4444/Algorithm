7e731fb8c39ed32fd1d9401ab0c8a8cdae18ac1f
kpd4444 <kpd4444@naver.com>
2026-08-11 18:30:08 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = false;
+        int sum = 0;
+        int temp = x;
+        while(temp > 0){
+            sum = sum + (temp % 10);
+            temp = temp / 10;
+        }
+        if(x % sum == 0){
+            answer = true;
+        }
+        
+        return answer;
+    }
+}

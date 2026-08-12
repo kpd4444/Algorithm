76b8ae93980807e1be78e13481e82b12bc94bf7b
kpd4444 <kpd4444@naver.com>
2026-08-12 18:16:47 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String s) {
+        int n = s.length();
+        if(n % 2 == 0){
+            return s.substring(n / 2 - 1, n / 2 + 1);
+        }else{
+            return s.substring(n / 2 , n / 2 + 1);
+        }
+        
+    }
+}

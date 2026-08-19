2f7a1b70b699aa43cfb856e67403870d018e4a93
kpd4444 <kpd4444@naver.com>
2026-08-19 17:09:49 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();        
+        
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == ' '){
+                sb.append(c);
+                count = 0;
+            }
+            else{
+                if(count % 2 == 0){
+                    sb.append(Character.toUpperCase(c));
+                    count++;
+                }else{
+                    sb.append(Character.toLowerCase(c));
+                    count++;
+                }
+            }
+            
+            
+        }
+        return sb.toString();
+    }
+}

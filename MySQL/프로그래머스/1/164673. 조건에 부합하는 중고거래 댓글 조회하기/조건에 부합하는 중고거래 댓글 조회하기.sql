86aad14fd12c75188b80b2c9480a19cbb5af3f33
kpd4444 <kpd4444@naver.com>
2026-09-01 13:51:40 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+select b.TITLE, b.BOARD_ID,	r.REPLY_ID, r.WRITER_ID, r.CONTENTS, r.CREATED_DATE
+from USED_GOODS_BOARD b
+join USED_GOODS_REPLY r on b.BOARD_ID = r.BOARD_ID
+where b.CREATED_DATE >= '2022-10-01' and b.CREATED_DATE < '2022-11-01'
+order by r.CREATED_DATE asc, b.TITLE asc;

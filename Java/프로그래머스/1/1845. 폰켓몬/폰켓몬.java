f79ae79372b7803e1313f2d73a58797b70431cdd
kpd4444 <kpd4444@naver.com>
2026-09-03 23:55:21 +0900

    [level 1] Title: 폰켓몬, Time: 5.52 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            set.add(nums[i]);
+        }
+        answer = set.size() > nums.length / 2 ? nums.length / 2 : set.size();
+        
+        return answer;
+    }
+}
